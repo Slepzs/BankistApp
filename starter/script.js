b71ecde85cff7a81c783fad74680abe7b4aa3a97
slepzs <slepzs@gmail.com>
2021-03-26 00:22:30 +0100

    advanced dom tech

--- starter/script.js
+++ starter/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,9 +19,9 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
 
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+  
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
@@ -29,3 +30,91 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+
+/////////////////////////////////////
+/////////////////////////////////////
+/////////////////////////////////////
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+
+const header  = document.querySelector('.header');
+// const allSection = document.querySelectorAll('.section');
+// console.log(allSection);
+
+// document.getElementById('section--1')
+// const allButtons = document.getElementsByTagName('button');
+// console.log(allButtons);
+
+// console.log(document.getElementsByClassName('btn'));
+
+
+// Creating and inserting elements
+// .insertAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+//message.textContent = 'We use cookies for improved functionality and analytics.';
+message.innerHTML = 'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it</button>';
+
+//header.prepend(message);
+header.append(message);
+//header.append(message.cloneNode(true));
+//header.insertAdjacentElement('afterend', message);
+
+//header.before(message)
+//header.after(message)
+
+document.querySelector('.btn--close-cookie').addEventListener('click', function() {
+  // New way
+  message.remove();
+  // Old way
+  //message.parentElement.removeChild(message);
+});
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+console.log(message.style.width);
+
+console.log(getComputedStyle(message).height);
+console.log(getComputedStyle(message).color);
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+// Non-standard
+//console.log(logo.designer);
+// Virker ikke fordi, jeg har ikke sat det på inde i doku.
+// console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist')
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+// Full URL eller kun det skrevet--
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes and
+logo.classList.add('c');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+
+// Don't use.. overskriver eksisterende klasser.. 
+logo.className = 'Tobias Heide';
